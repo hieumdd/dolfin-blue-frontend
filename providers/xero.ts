fefa4667678ffadf37abf2699e9554bfa4e2a02d
HM <hieumdd@gmail.com>
2022-06-16 22:47:56 +0700

    feat: Add Xero buildConsentUrl

---
+++ providers/xero.ts
@@ -0,0 +1,16 @@
+import { XeroClient } from 'xero-node';
+
+export const client = new XeroClient({
+    clientId: process.env.XERO_CLIENT_ID || '',
+    clientSecret: process.env.XERO_CLIENT_SECRET || '',
+    redirectUris: ['http://localhost:3000/api/callback/xero'],
+    scopes: [
+        'assets.read',
+        'accounting.settings.read',
+        'accounting.journals.read',
+        'accounting.budgets.read',
+        'accounting.reports.read',
+        'accounting.transactions.read',
+        'accounting.contacts.read',
+    ],
+});
